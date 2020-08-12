57d96fade512b73835a294ea2336b75def223a2b
James Rowe <jamesrowe@outlook.com>
2020-08-12 14:22:01 -0400

    fixed travis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
