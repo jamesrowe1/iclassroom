ac73644c80abe8b535e379e62caaee29f997fcf5
James Rowe <jamesrowe@outlook.com>
2020-08-13 12:37:39 -0400

    changes

--- models/document.js
+++ models/document.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
     const Document = sequelize.define("document", {
         // The email cannot be null, and must be a proper email before creation
         title: {
--- models/user.js
+++ models/user.js
@@ -26,22 +26,22 @@ module.exports = function(sequelize, DataTypes) {
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     role: {
       type: DataTypes.STRING,
-      allowNull: false,
-    },
+      allowNull: false
+    }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function (password) {
+  User.prototype.validPassword = function(password) {
     console.log(`${password} as opposed to ${this.password}`);
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function (user) {
+  User.addHook("beforeCreate", user => {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
