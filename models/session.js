fd78e67793b9a6c9095cfd9d263ba4167aacb9fc
Nathan Pecchia <septumprince@macbook-pro.home>
2020-08-13 12:31:44 -0400

    Added columns to User and Session models

--- models/session.js
+++ models/session.js
@@ -2,18 +2,54 @@ module.exports = function (sequelize, DataTypes) {
     const Session = sequelize.define("Session", {
         // The email cannot be null, and must be a proper email before creation
         event_title: {
-
+            type: DataTypes.STRING,
+            allowNull: false
         },
-        date: {
-
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [4]
         },
-        time: {
-
+        month: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [1, 2]
+        },
+        day: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [1, 2]
+        },
+        hour: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [1, 2]
+        },
+        minute: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [2]
         },
         attendees: {
             // Add foreign key for user.id
-
         }
     });
+
+    Session.associate = function (models) {
+        // We're saying that a Document should belong to an User
+        // A Document can't be created without an User due to the foreign key constraint
+        Session.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    Session.associate = function (models) {
+        // Associating Sessions with Users
+        Session.hasMany(models.User, {
+        });
+    };
+
     return Session;
 };
