2b5a4b827b5e585d2f334dfeb644ced940e70938
GCaplan <gabriel.caplan@gmail.com>
2020-08-14 13:58:58 -0400

    updated local config

--- config/config.js
+++ config/config.js
@@ -4,17 +4,17 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: "localhost",
-    dialect: "mysql",
+    dialect: "mysql"
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mysql"
   },
   production: {
     use_env_variable: "JAWSDB_URL",
-    dialect: "mysql",
-  },
+    dialect: "mysql"
+  }
 };
