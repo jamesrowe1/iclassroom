f7d546af197002ebc61292c9a8d4a5a42646c434
James Rowe <jamesrowe@outlook.com>
2020-08-14 14:32:54 -0400

    schedule

--- models/session.js
+++ models/session.js
@@ -41,10 +41,5 @@ module.exports = function(sequelize, DataTypes) {
     });
   };
 
-  // Session.associate = function(models) {
-  //   // Associating Sessions with Users
-  //   Session.hasMany(models.User, {});
-  // };
-
   return Session;
 };
