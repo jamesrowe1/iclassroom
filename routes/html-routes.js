048a86f8830ad6305b95f7782fa1eb9311428255
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-14 14:55:11 -0400

    ..

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -38,6 +38,10 @@ module.exports = function(app) {
     res.render("dashboard", { layout: "main" });
   });
 
+  app.get("/teacher-dashboard", (req, res) => {
+    res.render("teacher", { layout: "main" });
+  });
+  
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
