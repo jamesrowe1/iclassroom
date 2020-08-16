3fc819ab6cc2be321d9dcb1b5423d220e402796a
GCaplan <gabriel.caplan@gmail.com>
2020-08-16 19:22:16 -0400

    deleted schema file for creating seeds. Will revisit.

--- routes/schema.sql
+++
@@ -1,6 +0,0 @@
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS teacherreacher;
--- Creates the "blogger" database --
-CREATE DATABASE teacherreacher;
-
-INSERT INTO Document (client_name) VALUES ('Bilal');
