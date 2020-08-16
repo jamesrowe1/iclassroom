fae6b55e8c37e83b873b5b01e455a499c974ed4e
GCaplan <gabriel.caplan@gmail.com>
2020-08-16 19:19:59 -0400

    updated nav bar with mobile response

---
+++ routes/schema.sql
@@ -0,0 +1,6 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS teacherreacher;
+-- Creates the "blogger" database --
+CREATE DATABASE teacherreacher;
+
+INSERT INTO Document (client_name) VALUES ('Bilal');
